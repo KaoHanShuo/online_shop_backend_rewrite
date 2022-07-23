06f568726c1b34d15ea59481d8c9c526a3896539
Kao <a84090617@gmail.com>
2022-07-23 14:49:41 +0800

    用laravel寫購物網站後端，用api串接形式

---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/',function(){
+    return "api測試成功";
+});
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/accountCheck', [App\Http\Controllers\AuthController::class, 'accountCheck']);
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::get('/user-profile', [App\Http\Controllers\AuthController::class, 'userProfile']);
+});
+
+// Route::middleware('cors')->group(function (){
+//     Route::get('/', function(){
+//         return response()->json('跨域成功！',200);
+//     });
+// });
